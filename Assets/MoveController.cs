f82a09af53a7cb48032ae41a779c5fdab75e3269
Lukyanenko Evgeny <juz@hive.ru>
2021-10-26 00:29:26 +0500

    мы заставили кубик двигаться при нажатии кнопок wasdqe

---
+++ Assets/MoveController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [Range(0f, 0.1f)] public float offset;
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        CheckKeyPressedAndMove(KeyCode.W, Vector3.up);
+        CheckKeyPressedAndMove(KeyCode.S, Vector3.down);
+        CheckKeyPressedAndMove(KeyCode.A, Vector3.left);
+        CheckKeyPressedAndMove(KeyCode.D, Vector3.right);
+        CheckKeyPressedAndMove(KeyCode.E, Vector3.forward);
+        CheckKeyPressedAndMove(KeyCode.Q, Vector3.back);
+    }
+
+    private void CheckKeyPressedAndMove(KeyCode keyCode, Vector3 direction)
+    {
+        var keyPressed = Input.GetKey(keyCode);
+
+        if (keyPressed)
+        {
+            Move(direction);
+        }
+    }
+
+    private void Move(Vector3 moveDelta)
+    {
+        transform.position = transform.position + offset * moveDelta;
+    }
+}
